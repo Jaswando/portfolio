a08992db62cd8ef3a59e32ffe866ba8a6eba523a
Horace Keung <horacekeung@hotmail.com>
2018-07-19 23:05:20 +0100

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+	</div>
+</template>
+
+<script>
+export default {
+	head: {
+		title: 'About',
+		meta: [{ hid: 'description', name: 'description', content: 'About' }]
+	}
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+	</div>
+</template>
+
+<script>
+export default {
+	head: {
+		title: 'Work',
+		meta: [{ hid: 'description', name: 'description', content: 'Work' }]
+	}
+}
+</script>
