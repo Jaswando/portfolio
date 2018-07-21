f3ccfe031befe57c1cbd9ef9de9e2b3a9bf26c52
Horace Keung <horacekeung@hotmail.com>
2018-07-22 00:39:10 +0100

    try 404

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,10 @@ module.exports = {
 			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
 			{ hid: 'description', name: 'description', content: 'A Product Designer focused on creating digital experiences by solving human problems first to drive usable & scalable design systems' }
 		],
-		link: [{rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}]
+		link: [{rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}],
+		script: [
+			{ src: '/gh-spa.js' }
+		]
 	},
 	// Customize the progress bar color
 	loading: false,
