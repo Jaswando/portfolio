9b96ee3a340a0a31800f55879115a2a731f1c120
Horace Keung <horacekeung@hotmail.com>
2018-07-22 01:40:07 +0100

    added hero text

--- pages/work/_name.vue
+++ pages/work/_name.vue
@@ -1,7 +1,9 @@
 <template>
-	<div v-resize:debounce="onResize" :style="`background-image: url('../img/${name}/${heroSize}.png'); height: 34.375rem;`">
-		{{name}}
-		{{heroSize}}
+	<div v-resize:debounce="onResize" class="bg-center text-white lg:flex" :style="`background-image: url('../img/${name}/${heroSize}.png'); height: 34.375rem;`">
+		<div class="px-4 pt-4 lg:my-auto lg:w-64">
+			<p class="font-medium text-2xl mb-4">Football club app</p>
+			<p>Helping build lasing connections between fans and the clubs they support. I worked on building a whitelabel platform to house real time interactions and integrations.</p>
+		</div>
 	</div>
 </template>
 
