99d084b92a241d1253f1250dde4fc84101299a34
Horace Keung <horacekeung@hotmail.com>
2018-07-22 20:37:17 +0100

    added back to top

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,13 +3,19 @@
 		<page-header/>
 		<div class="flex-1"><nuxt/></div>
 		<page-footer/>
+		<back-to-top>
+			<div class="bg-blue w-12 h-12 rounded flex">
+				<img class="w-4 m-auto" src="~/assets/img/back-to-top.png">
+			</div>
+		</back-to-top>
 	</div>
 </template>
 
 <script>
 import PageHeader from '~/components/PageHeader'
 import PageFooter from '~/components/PageFooter'
+import BackToTop from 'vue-backtotop'
 export default {
-	components: {PageHeader, PageFooter}
+	components: {PageHeader, PageFooter, BackToTop}
 }
 </script>
