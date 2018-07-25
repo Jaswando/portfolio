c2e855e4d23f06097d655a05da790f481525fa87
Horace Keung <horacekeung@hotmail.com>
2018-07-25 01:10:29 +0100

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,7 @@ module.exports = {
 			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
 			{ hid: 'description', name: 'description', content: 'A Product Designer focused on creating digital experiences by solving human problems first to drive usable & scalable design systems' }
 		],
-		link: [{rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}],
+		link: [{rel: 'icon', type: 'image/x-icon', href: (process.env.DEPLOY_ENV === 'GH_PAGES' ? '/portfolio' : '') + '/favicon.ico'}],
 		script: process.env.DEPLOY_ENV === 'GH_PAGES' ? [{src: '/portfolio/gh-spa.js'}] : [] 
 	},
 	// Customize the progress bar color
