20c744f05610e634c3fced26e2e15ed6e548c07d
Horace Keung <horacekeung@hotmail.com>
2018-07-28 22:26:03 +0100

    landing page work animate when in viewport

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
 	<div class="container">
 		<p class="animated fadeIn py-32 md:py-48 sm:mx-12 lg:w-2/3 text-2xl md:text-4xl leading-normal">I'm a <span class="text-blue">Product Designer</span> focused on creating digital experiences and solving human problems to drive usable & scalable design systems.</p>
 		<div class="sm:flex flex-wrap -mx-2 mb-8">
-			<div v-for="w in works" :key="w.name" class="sm:w-1/2 px-2 mb-4 animated fadeInUp">
+			<div v-for="(w,index) in works" :key="w.name" :id="w.name" v-observe-visibility="{callback:visibilityChanged,throttle:index*50+50}" :class="'sm:w-1/2 px-2 mb-4 '+(w.isVisible?'animated fadeInUp':'opacity-0')">
 				<nuxt-link :to="`/${w.name}`">
 					<div class="overflow-hidden cursor-pointer relative trigger">
 						<img class="w-full" :src="`./img/${w.name}.png`">
@@ -21,14 +21,21 @@
 
 <script>
 export default {
+	methods: {
+		visibilityChanged (isVisible, entry) {
+			if (isVisible) {
+				this.works.find(w => w.name === entry.target.id).isVisible = true
+			}
+		}
+	},
 	data: () => ({
 		works: [
-			{name: 'club-app', title: 'Club App', text: 'iOS & Android'},
-			{name: 'our-league', title: 'Rugby League Membership', text: 'iOS, Android & Web'},
-			{name: 'rewards', title: 'Aston Villa Rewards', text: 'iOS & Android'},
-			{name: 'tickets', title: 'Tickets', text: 'iOS & Android'},
-			{name: 'cast', title: 'InCrowd Cast', text: 'Windows Application'},
-			{name: 'dashboard', title: 'Insights Dashboard', text: 'Web'}
+			{name: 'club-app', title: 'Club App', text: 'iOS & Android', isVisible: false},
+			{name: 'our-league', title: 'Rugby League Membership', text: 'iOS, Android & Web', isVisible: false},
+			{name: 'rewards', title: 'Aston Villa Rewards', text: 'iOS & Android', isVisible: false},
+			{name: 'tickets', title: 'Tickets', text: 'iOS & Android', isVisible: false},
+			{name: 'cast', title: 'InCrowd Cast', text: 'Windows Application', isVisible: false},
+			{name: 'dashboard', title: 'Insights Dashboard', text: 'Web', isVisible: false}
 		]
 	})
 }
--- plugins/main.js
+++ plugins/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
+import VueObserveVisibility from 'vue-observe-visibility'
 import VueImg from 'v-img'
 import Gallery from '~/components/generic/Gallery'
 import Hero from '~/components/generic/Hero'
 import Paragraph from '~/components/generic/Paragraph'
 import TextSection from '~/components/generic/TextSection'
+Vue.use(VueObserveVisibility)
 Vue.use(VueImg)
 Vue.component('gallery', Gallery)
 Vue.component('hero', Hero)
